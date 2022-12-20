3dafe76c6716686e384343c8d20ce355090915d9
rahultambe9890 <rahultambe9890@gmail.com>
2022-12-20 21:42:27 +0530

    loop.js
    loop.js

---
+++ 05_loop.js
@@ -0,0 +1,32 @@
+console.log(`---------------Assignment 0B -----------------`);
+console.log(`.1)`);
+for ( let index=5; index<15; index++){
+console.log(index);
+}
+console.log(`.2)`);
+for ( let index=50; index>=40; index--){
+console.log(index);
+}
+console.log(`.3)`);
+for ( let index=1; index<=31; index+=2){
+console.log(index);
+}
+console.log(`.4)`);
+for ( let index=2; index<22; index+=2){
+console.log(index);
+}
+console.log(`.5)`);
+
+for ( let index=1; index<=10; index++){
+    var table = 5
+console.log(index*table);
+}
+console.log(`.6)`);
+for ( let index=1; index<=10; index++){
+    var table = 10
+console.log(index*table);
+}
+console.log(`.7)`);
+for ( let index=100; index>=10; index-=10){
+console.log(index);
+}
