08b7aa364771c67dbbb78225ed35c7a16cb38e16
rahultambe9890 <rahultambe9890@gmail.com>
2022-12-27 07:12:03 +0530

    whileLoop.js
    whileLoop.js

---
+++ 05_whileLoop.js
@@ -0,0 +1,44 @@
+console.log(`---------------Assignment 01 -----------------`);
+console.log(`.1)`);
+var index= 5 ;
+while (index <=15) { 
+    console.log(index); 
+    index++;
+}
+console.log(`.2)`);
+var index= 50;
+while (index >=40) {
+    console.log(index); 
+    index--;
+}
+console.log(`.3)`);
+var index= 1;
+while (index <= 31) {
+    console.log(index); 
+    index+=2;
+}
+console.log(`.4)`);
+var index=2;
+while (index<=22) {
+    console.log(index); 
+    index+=2;
+}
+console.log(`.5)`);
+
+var index =5;
+do {
+    console.log(index);
+    index+=5;
+} while (index<=50);
+console.log(`.6)`);
+var index =10;
+do {
+    console.log(index);
+    index+=10;
+} while (index<=100);
+console.log(`.7)`);
+var index =100;
+do {
+    console.log(index);
+    index-=10;
+} while (index>=10);
