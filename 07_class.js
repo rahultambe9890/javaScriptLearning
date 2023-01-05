9f3be6dece9f1a0031cca197eff12256ce2d0e86
rahultambe9890 <rahultambe9890@gmail.com>
2023-01-05 08:20:15 +0530

    class
    class

---
+++ 07_class.js
@@ -0,0 +1,15 @@
+class Employee{
+    constructor(name, id, age, city){
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.city = city;
+    } 
+}
+let empSmith = new Employee("Smith", 33, 26, "Delhi");
+let empJay = new Employee("Jay", 3000, 21, "Pune");
+let empBill = new Employee("Billgates", 5000, 58, "USA");
+console.log(empSmith);
+console.log(empJay);
+console.log(empBill);
+
