79fe5cf54476c6c6bdfc0097de24a517f88497f1
rahultambe9890 <rahultambe9890@gmail.com>
2023-01-07 08:12:23 +0530

    hoisting.js
    hoisting.js

---
+++ 10_hoisting.js
@@ -0,0 +1,18 @@
+console.log(city);
+
+var city = "Pune";
+
+
+let state = "MH";
+console.log(state);
+
+show();
+
+function show(){
+    console.log("Hello I am in show");
+}
+
+display();
+var display = function(){
+    console.log("Hello I am in display");
+}
