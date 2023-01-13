c2fefa973d1a1dee112cea0bb63dac21b5374b03
rahultambe9890 <rahultambe9890@gmail.com>
2023-01-13 20:44:04 +0530

    usetStrict.js
    usetStrict.js

---
+++ 11_usetStrict.js
@@ -0,0 +1,16 @@
+'use strict'
+let city = "Pune";
+console.log(city);
+
+const person = {
+    name: "Mohit",
+    age: 23
+}
+console.log(person);
+
+// delete person;
+
+function show(name, name){
+    console.log(name, name);
+}
+show("Dhoni", 42);
